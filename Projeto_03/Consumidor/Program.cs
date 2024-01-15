c02ea928802a69a235c37ffa66769e64817b8aa3
italo pessan <italopessan@gmail.com>
2024-01-15 15:04:03 -0300

    um publicador e varios consumidores

---
+++ Projeto_03/Consumidor/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var servidor = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "usuario",
+    Password = "senha@123",
+};
+
+var conexao = servidor.CreateConnection();
+{
+
+    using (var canal = conexao.CreateModel())
+    {
+
+        canal.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+        var nomeFila = canal.QueueDeclare().QueueName;
+
+        canal.QueueBind(queue: nomeFila, exchange: "logs", routingKey: "");
+
+        Console.WriteLine(" [*] Aguardando mensagens.");
+
+        var consumidor = new EventingBasicConsumer(canal);
+
+        consumidor.Received += (model, ea) =>
+        {
+           
+            var corpo = ea.Body.ToArray();
+           
+            var mensagem = Encoding.UTF8.GetString(corpo);
+
+            Console.WriteLine(" [x] Recebido {0}", mensagem);
+
+        };
+
+        canal.BasicConsume(queue: nomeFila, autoAck: true, consumer: consumidor);
+
+        Console.WriteLine(" Pressione [enter] para finalizar.");
+        Console.ReadLine();
+    }
+
+}
+
---
+++ Projeto_03/Publicador/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var servidor = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "usuario",
+    Password = "senha@123",
+};
+
+//Conexão com o servidor
+var conexao = servidor.CreateConnection();
+{
+    //Canal
+    using (var canal = conexao.CreateModel())
+    {
+        //Criar uma Exchange
+        canal.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+        Console.WriteLine("Digite uma mensagem");
+
+        while (true)
+        {
+            //Mensagem
+            string mensagem = Console.ReadLine();
+
+            if (mensagem == "!finalizar") break;
+
+            //Convertendo a Mensagem em bytes
+            var corpoMensagem = Encoding.UTF8.GetBytes(mensagem);
+
+            //registrar mensagem
+            canal.BasicPublish(exchange: "logs",
+                                 routingKey: "",
+                                 basicProperties: null,
+                                 body: corpoMensagem);
+
+            Console.WriteLine(" [x] Enviou {0}", mensagem);
+        }
+    }
+
+    Console.WriteLine(" Pressione [enter] para sair.");
+    Console.ReadLine();
+}
