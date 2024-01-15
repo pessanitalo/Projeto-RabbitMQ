d6e0fffd570fdbc6475cbe9236828e44e7a7ef6a
italo pessan <italopessan@gmail.com>
2024-01-15 15:24:38 -0300

    Publicador  passando rotas diferentes

---
+++ Projeto_04/Publicador/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var servidor = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "usuario",
+    Password = "senha@123",
+};
+
+var conexao = servidor.CreateConnection();
+{
+
+    using (var canal = conexao.CreateModel())
+    {
+        canal.ExchangeDeclare(exchange: "mensagem_empresa", type: ExchangeType.Direct);
+
+        Console.WriteLine("Digite uma mensagem");
+
+        while (true)
+        {
+            string mensagem = Console.ReadLine();
+
+            if (mensagem == "!finalizar") break;
+
+            var corpoMensagem = Encoding.UTF8.GetBytes(mensagem);
+
+            //pegar uma routkey
+            Console.WriteLine("Digite uma rota");
+            string rotaKey = Console.ReadLine();
+
+            canal.BasicPublish(exchange: "mensagem_empresa",
+                                 routingKey: rotaKey,
+                                 basicProperties: null,
+                                 body: corpoMensagem);
+
+            Console.WriteLine(" [x] Enviou {0}", mensagem);
+        }
+    }
+
+    Console.WriteLine(" Pressione [enter] para sair.");
+    Console.ReadLine();
+}
