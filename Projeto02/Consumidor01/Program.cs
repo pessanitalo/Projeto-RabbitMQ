0d48f99c24d9030bd44f016507e3749f4cda81ec
italo pessan <italopessan@gmail.com>
2024-01-15 14:43:25 -0300

    adicionado mais consumidores

---
+++ Projeto02/Consumidor01/Program.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var servidor = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "usuario",
+    Password = "senha@123",
+};
+
+var conexao = servidor.CreateConnection();
+{
+
+    using (var canal = conexao.CreateModel())
+    {
+
+        canal.QueueDeclare(queue: "fila_de_tarefas",
+                             durable: true,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+
+        canal.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+        Console.WriteLine(" [*] Aguardando mensagens.");
+
+        var consumidor = new EventingBasicConsumer(canal);
+
+        consumidor.Received += (model, ea) =>
+        {
+            var corpo = ea.Body.ToArray();
+
+            var mensagem = Encoding.UTF8.GetString(corpo);
+
+            Console.WriteLine(" [x] Recebido {0}", mensagem);
+
+            canal.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+        };
+
+        canal.BasicConsume(queue: "fila_de_tarefas",
+                             autoAck: false,
+                             consumer: consumidor);
+
+        Console.WriteLine(" Pressione [enter] para finalizar.");
+        Console.ReadLine();
+    }
+
+}
